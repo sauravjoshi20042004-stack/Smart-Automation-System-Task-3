8330abe77f44fb1e555143d8617a5b62b871bd11
sauravjoshi20042004-stack <sauravjoshi20042004@gmail.com>
2026-08-14 12:54:54 +0530

    Added smart_automation_system.ino

---
+++ smart_automation_system.ino
@@ -0,0 +1,27 @@
+const int ldrPin = A0;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int lightValue = analogRead(ldrPin);
+
+  Serial.print("Light Value: ");
+  Serial.println(lightValue);
+
+  if (lightValue > 500) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("Status: DARK - LED ON");
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("Status: BRIGHT - LED OFF");
+  }
+
+  Serial.println("--------------------");
+
+  delay(1000);
+}
